fd4c68f082066bb7ee7b3b34cdbbe241b9d9145a
RaviStimphil <97135286+RaviStimphil@users.noreply.github.com>
2023-01-24 16:26:40 -0600

    Heehee

---
+++ sketch.js
@@ -0,0 +1,15 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(255);
+  noStroke();
+  fill(0,0,255,80);
+  ellipse(150,230,200,200);
+  fill(255,0,0,80);
+  ellipse(200,142,200,200);
+  fill(0,255,0,80);
+  ellipse(250,230,200,200);
+  
+}
