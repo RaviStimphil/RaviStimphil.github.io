6b3204fa614ad354648032491ec8addf79984c84
RaviStimphil <97135286+RaviStimphil@users.noreply.github.com>
2023-01-24 16:31:53 -0600

    I am here

--- sketch.js
+++ sketch.js
@@ -3,13 +3,5 @@ function setup() {
 }
 
 function draw() {
-  background(255);
-  noStroke();
-  fill(0,0,255,80);
-  ellipse(150,230,200,200);
-  fill(255,0,0,80);
-  ellipse(200,142,200,200);
-  fill(0,255,0,80);
-  ellipse(250,230,200,200);
-  
+  background(220);
 }
